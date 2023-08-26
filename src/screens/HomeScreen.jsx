21b1621fe1e849fdf8c427602f803496fd1a7931
Shahidul <shahid.cse.19@gmail.com>
2023-08-26 11:57:07 +0600

    Version 1.0.0 Complete

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -6,23 +6,23 @@ import Todo from "../components/Todo";
 import { useState } from "react";
 
 function HomeScreen() {
-    const [search, setSearch] = useState('pending')
-    const getTodo = (search,todos) =>{
-        if(search === 'pending'){
+    const [status, setStatus] = useState('pending')
+    const getTodo = (status,todos) =>{
+        if(status === 'pending'){
           const result = todos?.filter(item => item.completed === false)
           return result ? [...result].reverse() : result
         }
-        if(search === 'complete'){
+        if(status === 'complete'){
           return  todos?.filter(item => item.completed === true)
         }
-        if(search === 'all'){
+        if(status === 'all'){
           return todos ? [...todos].reverse() : todos
         }
     }
   // eslint-disable-next-line no-unused-vars
   const { todos, isFetching } = useGetAllTodosQuery(undefined,{
     selectFromResult: ({data}) => ({
-      todos: getTodo(search, data?.todos)
+      todos: getTodo(status, data?.todos)
     })
   });
   ///console.log(...todos);
@@ -32,11 +32,21 @@ function HomeScreen() {
         <Loader />
       ) : (
         <>
-          <Hero search={search} setSearch={setSearch}/>
-          <div className="main-container" >
-            { todos ? todos?.map((todo)=> <Todo todo={todo} key={todo._id}/>) : <h1 className="text-center">Welcome</h1>
+          <Hero status={status} setStatus={setStatus}/>
+          <div className="main-container">
+          { todos ? todos?.map((todo)=> <Todo todo={todo} key={todo._id}/>) : <h1 className="text-center">Welcome</h1>
             }
           </div>
+          {/* <div className="main-container" >
+            { todos ? todos?.map((todo)=> <Todo todo={todo} key={todo._id}/>) : 
+            ( userInfo ? (
+            <>
+               <h1 className="text-center">Welcome</h1>
+               <h4 className="text-center">Please Login and write your todo.</h4>
+            </>
+            ) : <h1 className="text-center">Add your First Todo.</h1>)
+            }
+          </div> */}
         </>
       )}
     </>
